52beb8fe70d067c07e1e7e5f792b8f1f9e289629
Stuart Lee <stuart.lee@drpgroup.com>
2019-05-08 21:55:17 +0100

    empty tiles are solid

--- src/engine/createWorld.js
+++ src/engine/createWorld.js
@@ -20,20 +20,23 @@ export default function createWorld (map) {
 
   for (let y = 0; y < map.height; y++) {
     for (let x = 0; x < map.width; x++) {
-      const ix = (y * map.width) + x
-      const tile = map.data[ix]
-      if (tile === 0) continue
+      const tile = get(map, x, y)
       const dx = x * TILE_SIZE
       const dy = y * TILE_SIZE
+      if (tile === 0) {
+        // if any surrounding tiles are solid, make it collidable
+        const surroundingTiles = getSurrounding(map, x, y)
+        if (surroundingTiles.some(tile => tile === 1)) {
+          console.log({ x, y, surroundingTiles })
+          const collider = makeCollider(dx, dy)
+          colliders.push(collider)
+        }
+        continue
+      }
       let mesh
       if (tile === 2) {
         mesh = wallMesh.clone()
-        const collider = new Rect(
-          dx - TILE_SIZE / 2,
-          dy - TILE_SIZE / 2,
-          TILE_SIZE,
-          TILE_SIZE
-        )
+        const collider = makeCollider(dx, dy)
         colliders.push(collider)
       } else {
         mesh = floorMesh.clone()
@@ -47,6 +50,29 @@ export default function createWorld (map) {
   Physics.setColliders(colliders)
 
   return world
+}
+
+function get (map, x, y) {
+  const ix = (y * map.width) + x
+  return map.data[ix]
+}
+
+const offsets = [[-1, -1], [0, -1], [1, -1], [-1, 0], [1, 0], [-1, 1], [0, 1], [1, 1]]
+
+function getSurrounding (map, x, y) {
+  return offsets
+    .map(([ox, oy]) => [ox + x, oy + y])
+    .filter(([ox, oy]) => (ox >= 0 && ox < map.width && oy >= 0 && oy < map.height))
+    .map(([ox, oy]) => get(map, ox, oy))
+}
+
+function makeCollider (x, y) {
+  return new Rect(
+    x - TILE_SIZE / 2,
+    y - TILE_SIZE / 2,
+    TILE_SIZE,
+    TILE_SIZE
+  )
 }
 
 const loader = new TextureLoader()
