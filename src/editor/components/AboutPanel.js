d9ed5d85d2453d5ebc3447d485b8e5b3c7ecd269
Stuart Lee <stuart.lee@drpgroup.com>
2019-05-16 22:27:04 +0100

    typo

--- src/editor/components/AboutPanel.js
+++ src/editor/components/AboutPanel.js
@@ -7,7 +7,7 @@ export default function AboutPanel () {
       <h2>What is this?</h2>
       <p>Tiny Little Worlds is a set of tools for creating small first person games, think <a href="https://ledoux.itch.io/bitsy">bitsy</a>, but in 3D</p>
       <p>It&apos;s pretty early at this point, but being actively developed. New features are being added all the time, so keep checking back!</p>
-      <p>If you find any bugs or have any ideas or suggestions, either let me know on <a href="http://twitter.com/beeglebug">twitter</a> or raise an issue on <a href="http://github.com/beeglebug/tiny-little-world">github</a> and i&apos;ll do my best!</p>
+      <p>If you find any bugs or have any ideas or suggestions, either let me know on <a href="http://twitter.com/beeglebug">twitter</a> or raise an issue on <a href="http://github.com/beeglebug/tiny-little-worlds">github</a> and i&apos;ll do my best!</p>
     </Panel>
   )
 }
