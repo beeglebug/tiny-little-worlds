5243f36eb1a20d42ab22c0dd23c65ea6bf58bfd5
Stuart Lee <stuart.lee@drpgroup.com>
2019-05-30 13:07:40 +0100

    tweak build and static server

--- src/server/index.js
+++ src/server/index.js
@@ -3,9 +3,7 @@ import express from 'express'
 const port = process.env.PORT || 3000
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send(JSON.stringify({ Hello: 'World' }))
-})
+app.use(express.static('dist'))
 
 app.listen(port, function () {
   console.log('listening on port', port)
