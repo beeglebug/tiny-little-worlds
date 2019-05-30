c0007c5528e16fc748ea2e6dd910b167c58753fd
Stuart Lee <stuart.lee@drpgroup.com>
2019-05-30 23:21:02 +0100

    add login to header

--- src/editor/components/Header.js
+++ src/editor/components/Header.js
@@ -5,8 +5,16 @@ import WindowMenu from './WindowMenu'
 export default function Header () {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Tiny Little Worlds</h1>
-      <WindowMenu />
+      <div className={styles.left}>
+        <h1 className={styles.title}>Tiny Little Worlds</h1>
+      </div>
+      <div className={styles.right}>
+        <WindowMenu />
+        <a
+          href="/login"
+          style={{ marginLeft: 10 }}
+        >login</a>
+      </div>
     </div>
   )
 }
