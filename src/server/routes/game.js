14e1d51b82262e11c141fbf418b6f461ce786618
Stuart Lee <stuart.lee@drpgroup.com>
2019-06-02 00:30:45 +0100

    set up some more routes

--- src/server/routes/game.js
+++ src/server/routes/game.js
@@ -1,6 +1,11 @@
+import React from 'react'
+import { renderToString } from 'react-dom/server'
 import express from 'express'
-import Game from '../models/Game'
 import User from '../models/User'
+import Game from '../models/Game'
+import html from '../templates/index.html'
+import GamePage from '../components/GamePage'
+import EditorPage from '../components/EditorPage'
 
 const router = express.Router()
 
@@ -24,6 +29,8 @@ async function createGame (request, response) {
   response.send('ok')
 }
 
+// TODO middleware to get user and game from url params
+
 async function getGame (request, response) {
 
   const { username, slug } = request.params
@@ -36,11 +43,40 @@ async function getGame (request, response) {
     return response.sendStatus(404)
   }
 
-  response.json(game)
+  const content = renderToString(
+    <GamePage
+      user={user}
+      game={game}
+    />
+  )
+
+  response.send(html(content))
 }
 
-router.get('/test', createGame)
+async function getGameEditor (request, response) {
 
+  const { username, slug } = request.params
+
+  const user = await User.findOne({ username }).exec()
+
+  const game = await Game.findOne({ slug, user: user._id }).exec()
+
+  if (game === null) {
+    return response.sendStatus(404)
+  }
+
+  const content = renderToString(
+    <EditorPage
+      user={user}
+      game={game}
+    />
+  )
+
+  response.send(html(content))
+}
+
+// router.get('/test', createGame)
 router.get('/:username/:slug', getGame)
+router.get('/:username/:slug/edit', getGameEditor)
 
 export default router
